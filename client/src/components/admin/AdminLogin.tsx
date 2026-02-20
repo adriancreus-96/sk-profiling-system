cdf022cc5696f1cc3fe090d2552c76044de10b9e
Adrian <main.adrian.creus@cvsu.edu.ph>
2026-02-20 19:45:18 +0800

    admin debug

--- client/src/components/admin/AdminLogin.tsx
+++ client/src/components/admin/AdminLogin.tsx
@@ -4,6 +4,7 @@ import { Shield } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 
 const API_URL = import.meta.env.VITE_API_URL
+console.log('🔍 API_URL:', API_URL); 
 
 const AdminLogin = () => {
   const [username, setUsername] = useState('');
